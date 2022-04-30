35230687726a7f1a96094adfc4f5aa355b4ea883
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-04-30 02:49:01 -0500

    feat: configurada prueba solicitada

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -2,16 +2,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { CafeListComponent } from './cafe-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CafeService } from '../cafe.service';
+import { Cafe } from '../cafe';
+import { Element } from '@angular/compiler';
+import { elementAt } from 'rxjs';
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CafeListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ CafeListComponent ],
+      providers: [ CafeService ]
     })
     .compileComponents();
   }));
@@ -19,10 +28,48 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    component.cafes = [
+      new Cafe(
+        faker.datatype.number(),
+        faker.name.findName(),
+        faker.lorem.word(),
+        faker.address.county(),
+        faker.word.adjective(),
+        faker.datatype.float(),
+        faker.image.imageUrl()
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.name.findName(),
+        faker.lorem.word(),
+        faker.address.county(),
+        faker.word.adjective(),
+        faker.datatype.float(),
+        faker.image.imageUrl()
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.name.findName(),
+        faker.lorem.word(),
+        faker.address.county(),
+        faker.word.adjective(),
+        faker.datatype.float(),
+        faker.image.imageUrl()
+      )
+    ];
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create table with header and 3 coffee varieties rows', () => {
+    expect(debug.queryAll(By.css('table thead tr')).length).toBe(1);
+    expect(debug.queryAll(By.css('table tbody tr')).length).toBe(3);
+    expect(debug.query(By.css('table tbody tr td')).nativeElement.textContent).toEqual(component.cafes[0].nombre);
   });
 });
